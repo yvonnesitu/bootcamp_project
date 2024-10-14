f38125367afbfaad64844a06c0c944fae8dfa47f
yvonnesitu <yvonne_ssy@yahoo.com.sg>
2024-10-14 20:33:55 +0800

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("AI Bootcamp - Capstone Assignment")
-st.write("Webapp created by: Yvonne Szeto (CPF Board)")
-
-st.image("background.jpg")
-st.caption("Image credit: ChatGPT")
