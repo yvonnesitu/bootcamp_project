595c56d1073f9fa5e41e26c1bbc851b14c099ff3
yvonnesitu <yvonne_ssy@yahoo.com.sg>
2024-10-14 17:37:37 +0800

    Add files via upload

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -4,7 +4,6 @@ from openai import OpenAI
 from crewai import Agent, Task, Crew
 from crewai_tools import WebsiteSearchTool
 import pandas as pd
-import tiktoken
 
 
 load_dotenv('.env')
@@ -13,34 +12,34 @@ load_dotenv('.env')
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 
-def generate_response_to_user_query(user_input):
+def generate_response_to_query(user_input):
 
     tool_websearch1 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/housing-loan-options/housing-loan-from-hdb")
     tool_websearch2 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/housing-loan-options/housing-loan-from-financial-institutions")
+    tool_websearch3 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/application-for-an-hdb-flat-eligibility-hfe-letter")
 
     qa_agent = Agent(
         role="Q&A assistant",
         goal="Write a relevant and factually accurate response to a flat buyer's query from HDB: {user_input}",
 
-        backstory="""As a customer service officer, you are incredibly resourceful in searching for relevant information and engage customers with a huge dose of empathy and an ever-cheerful attitude.
-        You're writing a response to a flat buyer's query: {user_input}.
-        You collect information from HDB's official website on what are the different housing loan options in order to answer flat buyer's query.
-        If the answer can be found on HDB's official website, include the URL and quote the exact words from which the information was extracted from.
-        If the answer cannot be found on HDB's official website, respond that you are unable to assist with the query.""",
-        tools = [tool_websearch1, tool_websearch2],
+        backstory="""As a customer service officer, you are incredibly resourceful in searching for relevant information. You engage customers with a warm, friendly demeanor, and a generous dose of empathy and cheerfulness. 
+        You're writing a response to a flat buyer's query: {user_input}. 
+        You collect information from HDB's official website on the various housing loan options available to assist the flat buyer. 
+        If you find the answer on HDB's official website, include the URL and quote the exact words from which the information was extracted. 
+        If the answer cannot be found, respond with understanding and offer general advice, while gently encouraging the buyer to reach out again if they have further questions, without using formal greetings or sign-offs.""",
+        tools = [tool_websearch1, tool_websearch2, tool_websearch3],
         allow_delegation=False,
         verbose=False,
     )
 
     task = Task(
         description="""\
-        1. Write a response to the flat buyer's query: {user_input}.
-        2. Proofread for grammatical errors.
-        3. Use bolding to emphasize key words and phrases.
-        4. Adopt a soothing and pleasant tone in the response, especially if the query cannot be answered.
-        """,
-
-        expected_output="A factually accurate and pleasant response to the query.",
+            1. Write a thoughtful response to the flat buyer's query: {user_input}.
+            2. Proofread the response for grammatical errors and clarity.
+            3. Use bolding to emphasize key words and phrases.
+            4. Ensure the response is polite but does not include salutations or sign-offs.
+            5. If the query cannot be answered, respond pleasantly and encourage the user to check the HDB website for more details, avoiding any formal greetings or closures.""",
+        expected_output="A factually accurate and pleasant response to the query, well-formatted and free of grammatical errors.",
         agent=qa_agent
     )
 
@@ -52,16 +51,25 @@ def generate_response_to_user_query(user_input):
 
     return crew.kickoff(inputs={"user_input": user_input})
 
-def generate_options(min, max, area):
+def get_price():
 
-    min_value = float(min)
-    max_value = float(max)
+    df = pd.read_csv("Resale flat prices based on registration date from Nov 2023 onwards.csv")
+
+    max = int(df['resale_price'].max())
+
+    return max
+
+def generate_options(min, max, area, flat_types):
     
     # Read the CSV file
     df = pd.read_csv("Resale flat prices based on registration date from Nov 2023 onwards.csv")
 
-    filtered_df = df[(df['town'] == area) & (df['resale_price'] >= min_value) & (df['resale_price'] <= max_value)]  # Replace accordingly
+    # Assuming min and max are either None or float values
+    filtered_df = df[
+        ((area == "None") | (df['town'] == area)) & 
+        ((min is None or df['resale_price'] >= min) if min is not None else True) & 
+        ((max is None or df['resale_price'] <= max) if max is not None else True) &
+        (df['flat_type'].isin(flat_types) if flat_types else True)
+    ]
 
     return filtered_df
-
-
