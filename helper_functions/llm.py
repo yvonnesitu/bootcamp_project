bc77b099f90a37070af2cc8ba8bac4be6256c466
yvonnesitu <yvonne_ssy@yahoo.com.sg>
2024-10-24 15:51:38 +0800

    Add files via upload

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -14,9 +14,11 @@ client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 def generate_response_to_query(user_input):
 
-    tool_websearch1 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/housing-loan-options/housing-loan-from-hdb")
-    tool_websearch2 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/housing-loan-options/housing-loan-from-financial-institutions")
-    tool_websearch3 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/understanding-your-eligibility-and-housing-loan-options/application-for-an-hdb-flat-eligibility-hfe-letter")
+    tool_websearch1 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/buying-procedure-for-resale-flats")
+    tool_websearch2 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/buying-procedure-for-resale-flats/overview")
+    tool_websearch3 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/buying-procedure-for-resale-flats/plan-source-and-contract")
+    tool_websearch4 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/buying-procedure-for-resale-flats/resale-application")
+    tool_websearch5 = WebsiteSearchTool("https://www.hdb.gov.sg/cs/infoweb/residential/buying-a-flat/buying-procedure-for-resale-flats/resale-completion")
 
     qa_agent = Agent(
         role="Q&A assistant",
@@ -27,7 +29,7 @@ def generate_response_to_query(user_input):
         You collect information from HDB's official website on the various housing loan options available to assist the flat buyer. 
         If you find the answer on HDB's official website, include the URL and quote the exact words from which the information was extracted. 
         If the answer cannot be found, respond with understanding and offer general advice, while gently encouraging the buyer to reach out again if they have further questions, without using formal greetings or sign-offs.""",
-        tools = [tool_websearch1, tool_websearch2, tool_websearch3],
+        tools = [tool_websearch1, tool_websearch2, tool_websearch3, tool_websearch4, tool_websearch5],
         allow_delegation=False,
         verbose=False,
     )
@@ -59,17 +61,44 @@ def get_price():
 
     return max
 
-def generate_options(min, max, area, flat_types):
+def generate_options(txn, min, max, area, flat_types):
     
     # Read the CSV file
     df = pd.read_csv("Resale flat prices based on registration date from Nov 2023 onwards.csv")
 
-    # Assuming min and max are either None or float values
-    filtered_df = df[
+    if txn == 1:
+        # month starts from "2024-05"
+        filtered_df = df[
+        (df['month'] >= "2024-05") & 
         ((area == "None") | (df['town'] == area)) & 
         ((min is None or df['resale_price'] >= min) if min is not None else True) & 
         ((max is None or df['resale_price'] <= max) if max is not None else True) &
         (df['flat_type'].isin(flat_types) if flat_types else True)
     ]
+    elif txn == 2:
+        # month starts from "2024-08"
+        filtered_df = df[
+        (df['month'] >= "2024-08") & 
+        ((area == "None") | (df['town'] == area)) & 
+        ((min is None or df['resale_price'] >= min) if min is not None else True) & 
+        ((max is None or df['resale_price'] <= max) if max is not None else True) &
+        (df['flat_type'].isin(flat_types) if flat_types else True)
+    ]
+    else:
+        # month starts from "2023-11"
+        filtered_df = df[
+        ((area == "None") | (df['town'] == area)) & 
+        ((min is None or df['resale_price'] >= min) if min is not None else True) & 
+        ((max is None or df['resale_price'] <= max) if max is not None else True) &
+        (df['flat_type'].isin(flat_types) if flat_types else True)
+    ]
+
+    # filtered_df = df[
+    #     (df['month'] < "2024-05") & 
+    #     ((area == "None") | (df['town'] == area)) & 
+    #     ((min is None or df['resale_price'] >= min) if min is not None else True) & 
+    #     ((max is None or df['resale_price'] <= max) if max is not None else True) &
+    #     (df['flat_type'].isin(flat_types) if flat_types else True)
+    # ]
 
     return filtered_df
